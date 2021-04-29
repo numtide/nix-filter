803e07809312b081701eb9a56ccc64b676527839
zimbatm <zimbatm@zimbatm.com>
2021-04-29 19:49:26 +0200

    proposed change

--- default.nix
+++ default.nix
@@ -17,52 +17,45 @@ rec {
     , # Ignore the following matches
       exclude ? [ ]
     }:
-    let
-      rootStr = toString root;
+      assert builtins.isPath root;
+      let
+        rootStr = toString root;
 
-      # If an argument to include or exclude is a path, transform it to a matcher.
-      #
-      # This probably needs more work, I don't think that it works on
-      # sub-folders.
-      toMatcher = f:
-        let
-          # Push these here to memoize the result
-          path_ = toString f;
-          path__ = "${rootStr}/${f}";
-        in
-        if builtins.isFunction f then f
-        else if builtins.isPath f then (_: path: _: path_ == path)
-        else if builtins.isString f then (_: path: _: path__ == path)
-        else
-          throw "Unsupported type ${builtins.typeOf f}";
+        # If an argument to include or exclude is a path, transform it to a matcher.
+        #
+        # This probably needs more work, I don't think that it works on
+        # sub-folders.
+        toMatcher = f:
+          let
+            path_ = _toCleanPath root f;
+          in
+          if builtins.isFunction f then f root
+          else
+            (path: _: path_ == path);
 
-      include_ = map toMatcher include;
-      exclude_ = map toMatcher exclude;
-    in
-    builtins.path {
-      inherit name;
-      path = root;
-      filter = path: type:
-        (builtins.any (f: f root path type) include_) &&
-        (!builtins.any (f: f root path type) exclude_);
-    };
+        include_ = map toMatcher include;
+        exclude_ = map toMatcher exclude;
+      in
+      builtins.path {
+        inherit name;
+        path = root;
+        filter = path: type:
+          (builtins.any (f: f path type) include_) &&
+          (!builtins.any (f: f path type) exclude_);
+      };
 
   # Match a directory and any path inside of it
-  inDirectory = directory:
-    root: path: type:
-      let
-        # Convert `directory` to a path to clean user input
-        dirAsPath =
-          if builtins.isString directory then root + ("/" + directory)
-          else if builtins.isPath directory then directory
-          else
-            throw "inDirectory: Unsupported type ${builtins.typeOf directory}, expected string or path";
-        directory_ = toString dirAsPath;
-        path_ = toString path;
-      in
-      directory_ == path_
-      # Add / to the end to make sure we match a full directory prefix
-      || _hasPrefix (directory_ + "/") path_;
+  inDirectory =
+    directory:
+    root:
+    let
+      # Convert `directory` to a path to clean user input.
+      directory_ = _toCleanPath root directory;
+    in
+    path: type:
+    directory_ == path
+    # Add / to the end to make sure we match a full directory prefix
+    || _hasPrefix (directory_ + "/") path;
 
   # Match paths with the given extension
   matchExt = ext:
@@ -84,6 +77,20 @@ rec {
     builtins.trace "unmatched path=${path} type=${type}" false;
 
   # Lib stuff
+
+  # Makes sure a path is:
+  # * absolute
+  # * doesn't contain superfluous slashes or ..
+  #
+  # Returns a string so there is no risk of adding it to the store by mistake.
+  _toCleanPath = absPath: path:
+    assert builtins.isPath absPath;
+    if builtins.isPath path then
+      toString path
+    else if builtins.isString path then
+      toString (absPath + ("/" + path))
+    else
+      throw "unsupported type ${builtins.typeOf path}, expected string or path";
 
   _hasSuffix =
     # Suffix to check for
