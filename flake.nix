41fd48e00c22b4ced525af521ead8792402de0ea
zimbatm <zimbatm@zimbatm.com>
2023-09-16 11:48:56 +0200

    expose lib as a functor
    This allows calling the nix-filter input directly. Eg:
    
        outputs = { self, nix-filter }:
          ...
          src = nix-filter {
            root = self;
    
            includes = [
              ./dir
            ];
          };

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "nix-filter";
 
   outputs = { self }: {
+    __functor = self.lib.__functor;
     lib = import ./default.nix;
     overlays.default = _: _: { nix-filter = self.lib; };
   };
